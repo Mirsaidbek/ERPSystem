52d1bad96d909b3c61824357a1ed8b7d10f9d27d
Mirsaidbek <69688827+Mirsaidbek@users.noreply.github.com>
2023-07-25 10:13:34 +0500

    Update MediaUtils.java

--- src/main/java/dev/said/firebase/MediaUtils.java
+++ src/main/java/dev/said/firebase/MediaUtils.java
@@ -5,7 +5,6 @@ public class MediaUtils {
     public final static String BUCKET_NAME = "erpsystm.appspot.com";
 
     public final static String DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/" + BUCKET_NAME + "/o/%s?alt=media";
-//                                               https://firebasestorage.googleapis.com/v0/b/test-935c1.appspot.com/o/2695cacd-e3d9-48dd-ba57-7fff54718dc1Screenshot+from+2022-05-09+12-10-42.png?alt=mediaa
     public final static String FIREBASE_TOKEN_PATH = "D:\\Pet Projects\\5.SpriingBoot\\ERPSystem\\src\\main\\resources\\credentials.json";
 
 
